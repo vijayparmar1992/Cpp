9d82c7cd8fdebe19d4560ea0273cb71b0c5d7679
Vijay Parmar <vijay.parmar1992@gmail.com>
2024-02-17 20:40:49 +0530

    Updated FunctionPointer.cpp

--- Pointers/FunctionPointer.cpp
+++ Pointers/FunctionPointer.cpp
@@ -1,7 +1,12 @@
-// This program demonstrates Function pointer;
+// Example of Function pointers
+// Use case of Function pointers : callback mechanism, dynamic function invocation
 #include <iostream>
 
 using namespace std;
+
+
+// Function pointer declaration
+int (*fp)(int, int) = NULL; // Always good to assign NULL 
 
 int add(int a, int b)
 {
@@ -13,39 +18,46 @@ int sub(int a, int b)
 	return a - b;
 }
 
-// Function pointer
-int (*operation)(int a, int b) = NULL; // Inialize to NULL
-
-//Func pointer as argument to function
-int operate(int (*fp)(int a, int b), int num1, int num2)
+// Passing function pointer as argument to function
+int operate(int (*fp)(int, int), int a, int b)
 {
-	return fp(num1, num2);
+	return fp(a, b);
 }
 
-//Array of Function pointer
-int (*funcs[2])(int a, int b) = { NULL };
+// Array of function pointers
+int (*fpArray[10])(int, int) = { NULL };
 
 int main()
 {
-	operation = add;
-	cout << "Addition is " << operation(6, 5) << endl;
+	// **Case1: Simple usage of function pointer
+	int a = 5; int b = 3;
+	// Assign address of function add()
+	fp = add;
+	int result = fp(a, b);
+	cout << result<<endl;
 
-	operation = sub;
-	cout << "Substraction is " << operation(6, 5) << endl;
+	// Assign address of function sub()
+	fp = sub;
+	result = fp(a, b);
+	cout << result << endl;
 
-	// Passing function pointer as argument to function
-	cout << "Addition result is " << operate(add, 10, 20) <<endl;
-	cout << "Substraction result is " << operate(sub, 10, 20) <<endl;
-	
-	// Array of Function pointer
-	funcs[0] = add;
-	funcs[1] = sub;
+	// **Case2: Passing function pointer as argument to function
+	cout<< operate(add, a, b)<<endl;
+	cout << operate(sub, a, b) << endl;
 
-	for(int (*func)(int a, int b) :funcs)
+	// **Case3: Array of function pointer
+	fpArray[0] = add;
+	fpArray[1] = sub;
+	a = 12;
+	fpArray[2] = add;
+	fpArray[3] = sub;
+	fpArray[4] = sub;
+
+	for (int i = 0; i < 5; i++)
 	{
-		cout << func(8, 5) << endl;
+		cout << operate(fpArray[i], a, b) << endl;
+		a = 5;
 	}
-
+	
 	return 0;
 }
-
