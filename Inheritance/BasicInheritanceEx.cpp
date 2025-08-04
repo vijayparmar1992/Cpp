ec9d22ae79841bce9d4d251dde8bbac0c295e5bf
Vijay Parmar <vijay.parmar1992@gmail.com>
2025-08-04 18:58:18 +0530

    Create BasicInheritanceEx.cpp

---
+++ Inheritance/BasicInheritanceEx.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+// Base class
+class Index {
+public:
+	int count;	
+	Index()
+	{
+		count = 0;
+	}
+
+	void operator ++()
+	{
+		count++;
+	}
+
+	void display()
+	{
+		cout << "count is " << count << endl;
+	}
+};
+
+// Derived class
+class NewIndex : public Index
+{
+public:
+	void operator  --()
+	{
+		count--;
+	}
+};
+
+int main()
+{
+	Index i;
+	++i;
+	i.display();
+	++i;
+	i.display();
+
+	NewIndex n;
+	++n;
+	++n;
+	n.display();
+	--n;
+	n.display();
+
+	return 0;
+}
